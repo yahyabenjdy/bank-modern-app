d8eff6c3280c4889e096cc6d80d9075919e15d6e
Yahya Benjdy <yahyabenjdy044@gmail.com>
2024-02-09 21:14:53 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
-  const [active, setActive] = useState("Home");
+  const [active, setActive] = useState("Home"); 
   const [toggle, setToggle] = useState(false);
 
   return (
